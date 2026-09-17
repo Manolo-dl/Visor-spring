379fb120f3708769e9fb3d6baf5032199ee54152
Manolo <manueldl12062003@gmail.com>
2026-09-17 11:46:01 +0200

    created usuario entity

--- src/main/java/es/sstrategy/crud_spring/entity/Tarea.java
+++ src/main/java/es/sstrategy/crud_spring/entity/Tarea.java
@@ -4,13 +4,17 @@ import java.time.LocalDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table(name = "tareas")
@@ -34,4 +38,9 @@ public class Tarea {
     private boolean completada = false;
 
     private LocalDate fechaLimite;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id")
+    @ToString.Exclude
+    private Usuario usuario;
 }
