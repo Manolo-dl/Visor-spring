22fa946313852de6dd027e431206cf8a37e18f88
Manolo <manueldl12062003@gmail.com>
2026-09-18 14:45:54 +0200

    fix update user

--- src/main/java/es/sstrategy/crud_spring/controller/UserController.java
+++ src/main/java/es/sstrategy/crud_spring/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -65,5 +66,17 @@ public class UserController {
             return ResponseEntity.noContent().<Void>build();
         })
         .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{email}")
+    public ResponseEntity<Void> actualizarUsuario(@PathVariable String email, @RequestBody UsuarioDTO usuarioDTO) {
+        return userRepository.findByEmail(email)
+            .map(usuario -> {
+                usuario.setNombre(usuarioDTO.nombre());
+                usuario.setEmail(usuarioDTO.email());
+                userRepository.save(usuario);
+                return ResponseEntity.noContent().<Void>build();
+            })
+            .orElse(ResponseEntity.notFound().build());
     }
 }
