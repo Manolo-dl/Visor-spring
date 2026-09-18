ead3457c4099fe29ccd7c1b567c54ea53ef5bbd3
Manolo <manueldl12062003@gmail.com>
2026-09-18 08:17:16 +0200

    added cors configuration

---
+++ src/main/java/es/sstrategy/crud_spring/config/WebConfig.java
@@ -0,0 +1,37 @@
+package es.sstrategy.crud_spring.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration 
+public class WebConfig {
+
+    @Bean 
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        
+        config.setAllowedHeaders(Arrays.asList("Authorization",
+            "Cache-Control",
+            "Content-Type",
+            "X-Requested-with",
+            "Accept",
+            "Origin"
+        ));
+
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
